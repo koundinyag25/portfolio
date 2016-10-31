5e335167d63cde0c0560c89b49ae0c137b40ea1b
koundinyag25 <koundinyag25@gmail.com>
2016-10-30 23:03:16 -0500

    noo

---
+++ src/home/home.controller.js
@@ -0,0 +1,28 @@
+function HomeController($scope,$location,$anchorScroll,scrollService,$timeout){
+  $scope.image = "src/svg/profile_pic.jpg";
+  $scope.arrow="src/svg/arrow.png";
+
+$scope.buttons = [ { name: 'Home', value: 'home'},{ name:'Family Tree', value:'family-tree'},{ name:'Gallery', value: 'gallery'},{name:'Contact Us', value:'contactus'}];
+ $scope.gotoSection = function(id) {
+      $location.hash(id);
+      scrollService.scrollTo(id);
+ };
+
+ $scope.gotoNextSection = function(){
+      if(event.target.getAttribute("value") == "family-tree"){
+        $scope.rotateClass = "rotate";
+        $location.hash("family-tree");
+          scrollService.scrollTo("family-tree");
+      }
+      if(event.target.getAttribute("value") == "gallery"){
+        console.log(event.target.getAttribute("value"));
+        scrollService.scrollTo("gallery");
+      }
+      if(event.target.getAttribute("value") == "contactus"){
+        $location.hash("contactus");
+          scrollService.scrollTo("contactus");
+      }
+ }
+}
+
+export default HomeController;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+
+const webpack = require('webpack');
+const path = require('path');
+
+
+module.exports = {
+  devtool : 'inline-source-nap',
+  entry:['webpack-dev-server/client?http://127.0.0.1:4000',
+        'webpack/hot/only-dev-server',
+        './src'
+      ],
+      output: {
+        path : path.join(__dirname,'public'),
+        filename: 'app.js'
+      },
+      devServer : {
+        inline: true,
+        port: 4000
+      },
+      resolve:{
+        moduleDirectories: ['node_modules','src'],
+        extensions:['','.js','.css']
+      },
+      module:{
+        loaders:[
+          {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loaders:['babel?presets[]=es2015']
+        }
+        // ,
+        // {
+        //     test: /\.css$/,
+        //     loaders: [
+        //         'style-loader','css-loader'
+        //     ]
+        // }
+    ]
+  },
+  plugins :[
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin()
+  ]
+}
